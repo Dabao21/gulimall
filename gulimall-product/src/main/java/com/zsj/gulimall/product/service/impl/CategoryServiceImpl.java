c2cb9a1df3390b50198f115f3533a1ab9a89df3e
zhang <642011598@qq.com>
2022-06-03 11:01:14 +0800

    sql 01

--- gulimall-product/src/main/java/com/zsj/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/zsj/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,11 @@
 package com.zsj.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -25,5 +29,39 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
         return new PageUtils(page);
     }
+
+    @Override
+    public List<CategoryEntity> listwithTree() {
+        //1.查出所有分类
+        List<CategoryEntity> categoryEntities = baseMapper.selectList(null);
+        //2.组装成父子的树形结构
+        List<CategoryEntity> collects = categoryEntities.stream().filter(categoryEntity ->
+                        categoryEntity.getParentCid() == 0).collect(Collectors.toList()).stream().
+                map(categoryEntity ->
+                {
+                    categoryEntity.setChildren(getChidrens(categoryEntity, categoryEntities));
+                    return categoryEntity;
+                }).sorted((categoryEntity01, categoryEntity02) -> {
+                    //菜单的排序
+                    return (categoryEntity01.getSort()==null?0:categoryEntity01.getSort()) - (categoryEntity02.getSort()==null?0:categoryEntity02.getSort());
+                }).collect(Collectors.toList());
+
+        return collects;
+    }
+
+    public List<CategoryEntity> getChidrens(CategoryEntity root,List<CategoryEntity> all){
+
+        List<CategoryEntity> collect = all.stream().filter(categoryEntity -> categoryEntity.getParentCid() == root.getCatId()).
+                map(categoryEntity -> {
+                    categoryEntity.setChildren(getChidrens(categoryEntity, all));
+                    return categoryEntity;
+
+                }).sorted((categoryEntity0, categoryEntity1) -> {
+                    return (categoryEntity0.getSort()==null?0:categoryEntity0.getSort()) - (categoryEntity1.getSort()==null?0:categoryEntity1.getSort());
+                }).collect(Collectors.toList());
+        return collect;
+
+    }
+
 
 }
